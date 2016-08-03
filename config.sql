8cbb560f594181e1fc608a640cd99cbc0fc890a6
Hardware <contact@meshup.net>
2016-08-03 21:52:59 +0200

    fix(sql): mail settings

--- config.sql
+++ config.sql
@@ -1,9 +1,9 @@
-INSERT INTO `flarum`.`settings`(`key`,`value`) VALUES
+INSERT INTO `{{ DB_NAME }}`.`settings`(`key`,`value`) VALUES
   ('mail_host','{{ MAIL_HOST }}'),
   ('mail_port','{{ MAIL_PORT }}'),
   ('mail_username','{{ MAIL_USER }}'),
   ('mail_password','{{ MAIL_PASS }}'),
   ('mail_encryption','{{ MAIL_ENCR }}');
 
-UPDATE `flarum`.`settings` SET `value`='smtp' WHERE `key`='mail_driver';
-UPDATE `flarum`.`settings` SET `value`='{{ MAIL_FROM }}' WHERE `key`='mail_from';
+UPDATE `{{ DB_NAME }}`.`settings` SET `value`='smtp' WHERE `key`='mail_driver';
+UPDATE `{{ DB_NAME }}`.`settings` SET `value`='{{ MAIL_FROM }}' WHERE `key`='mail_from';
