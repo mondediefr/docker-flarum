ca084a6d0e489a11d38b0f1980d67e48f05c69c0
Monnier <monnierant42@gmail.com>
2018-08-23 09:25:15 +0200

    test find command to see the file list

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -32,8 +32,12 @@ sed -i "s/<OPCACHE_MEMORY_LIMIT>/$OPCACHE_MEMORY_LIMIT/g" /etc/php7/conf.d/00_op
 
 # Set permissions
 if [ "$DO_CHMOD" = true ]; then
-  chown -R $UID:$GID /flarum /services /var/log /var/lib/nginx
+  # chown -R $UID:$GID /flarum /services /var/log /var/lib/nginx
 fi
+find /flarum ! -user $UID -print0
+find /services ! -user $UID -print0
+find /var/log ! -user $UID -print0
+find /var/lib/nginx ! -user $UID -print0
 
 cd /flarum/app
 
@@ -110,8 +114,11 @@ fi
 
 # Set permissions
 if [ "$DO_CHMOD" = true ]; then
-  chown -R $UID:$GID /flarum
+  # chown -R $UID:$GID /flarum
 fi
+find /flarum ! -user $UID -print0
+
+
 
 # RUN !
 exec su-exec $UID:$GID /bin/s6-svscan /services
